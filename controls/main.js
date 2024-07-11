bbf5183eab642ed4977ac724b414d58923c4430a
Monegatto <jp.monegatto@protonmail.com>
2024-07-11 12:26:16 -0300

    Adicionada tela de criação de páginas. -Tela home onde o usuário pode inserir a URL e conteudo da página. -Objeto que armazena todas as páginas criadas para serem exibidas na tanto na tela home quanto na tela de login. -Tela home informa qual usuário está logado -Verificação se os campos da nova página foram preenchidos e se a URL foi repetida

--- controls/main.js
+++ controls/main.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+let usuario
+const todasPaginas = {
+    paginaNova: []
+};
+
 router.get("/", (req, res) => {
 
-    res.render("index")
-})
-
-router.get("/login", (req, res) => {
-
-    res.render("login")
+    res.render("index", {todasPaginas: todasPaginas})
 })
 
 // Para controle de autenticação do usuário deve ser utilizado sessão
@@ -16,23 +16,15 @@ router.post("/login", (req, res) => {
 
     if(req.body.usuario != process.env.USUARIO || req.body.senha != process.env.SENHA){
 
-        aviso = "Usuário não encontrado"
-
-        res.render("login", {aviso: aviso})
+        res.render("index", {aviso: "Usuário não encontrado", todasPaginas: todasPaginas})
 
         req.session.destroy()
 
-        alert("Usuário não encontrado")
-
     }else{
 
-        aviso = "Usuário encontrado"
+        usuario = req.body.usuario
 
-        req.session.usuario = req.body.usuario
-
-        res.render("login", {aviso: aviso})
-
-        alert("Usuário encontrado")
+        return res.render("home", { usuario: usuario, todasPaginas: todasPaginas } )
 
     }
 
@@ -43,10 +35,41 @@ router.post("/logout", (req, res) => {
 
     req.session.destroy()
 
-    aviso = "Usuário deslogado"
+    return res.render("index", {aviso: "Usuário deslogado", todasPaginas: todasPaginas})
 
-    return res.render("index", {aviso: aviso})
+})
+
+//A rota que recebe os dados da nova página e os armazena em um objeto que guarda todas as páginas, verificando se os campos foram preenchidos e se a URL é repetida
+router.post("/criar", (req, res) => {
+
+    if(req.body.url == "" || req.body.conteudo == ""){
+
+        return res.render("home", {aviso: "Preencha todos os dados", usuario: usuario, todasPaginas: todasPaginas})
+    }
+    else if(todasPaginas.paginaNova.some(pagina => pagina.url == req.body.url)){
+        return res.render("home", {aviso: "Essa URL já foi usada antes", usuario: usuario, todasPaginas: todasPaginas})
+    }
+
+    todasPaginas.paginaNova.push({"url": req.body.url, "conteudo": req.body.conteudo});
+
+    return res.render("home", { usuario: usuario, todasPaginas: todasPaginas });
+})
+
+//A rota que recebe a url da página como parâmetro e renderiza o template usando o conteudo como argumento
+router.post("/:url", (req, res) => {
 
 })
 
 module.exports = router
+
+/*
+
+colocar a restrição de conteudo vazio e repetido no input
+
+fazer as páginas serem exibidas uma do lado da outra ao invés de em baixo
+
+tornar o link clicável
+
+fazer com que uma página seja criada corretamente com base no conteudo -> criar um tempate mustache vazio que é preenchido com o conteudo
+
+*/
